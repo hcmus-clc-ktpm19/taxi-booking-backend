bdded0d7f231f061c4e6a196fe950d0a8e60da5b
Nguyen Duc Nam <namworkmc@gmail.com>
2022-07-04 22:07:28 +0700

    Initialize project.

---
+++ callcenter-service/src/test/java/com/hcmus/callcenterservice/CallcenterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmus.callcenterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallcenterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
