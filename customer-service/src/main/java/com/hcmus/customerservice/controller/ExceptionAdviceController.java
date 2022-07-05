d3752267acb09c1489cfe98d992b86c8a78b65b2
Nguyen Duc Nam <namworkmc@gmail.com>
2022-07-05 22:41:32 +0700

    Removed UnauthorizedException.

--- customer-service/src/main/java/com/hcmus/customerservice/controller/ExceptionAdviceController.java
+++ customer-service/src/main/java/com/hcmus/customerservice/controller/ExceptionAdviceController.java
@@ -1,6 +1,5 @@
 package com.hcmus.customerservice.controller;
 
-import com.hcmus.customerservice.model.exception.UnauthorizedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,14 +11,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionAdviceController {
 
   private final Logger logger = LoggerFactory.getLogger(ExceptionAdviceController.class);
-
-  @ExceptionHandler(UnauthorizedException.class)
-  public ResponseEntity<String> handleTokenExpiredException(UnauthorizedException e) {
-    String message = e.getMessage();
-    logger.warn(message);
-
-    return ResponseEntity.status(HttpStatus.FORBIDDEN).body(message);
-  }
 
   @ExceptionHandler(Throwable.class)
   public ResponseEntity<String> handleTokenExpiredException(Throwable e) {
