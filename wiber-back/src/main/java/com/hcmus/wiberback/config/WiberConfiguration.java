711e55b36cb24e123a655b85e90e3b15990d3539
Nguyen Duc Nam <66512409+namworkmc@users.noreply.github.com>
2022-07-19 20:54:16 +0700

    Change redis image in docker-compose.yaml (remember to run docker compose). Add redis cache manager (#20)

--- wiber-back/src/main/java/com/hcmus/wiberback/config/WiberConfiguration.java
+++ wiber-back/src/main/java/com/hcmus/wiberback/config/WiberConfiguration.java
@@ -1,14 +1,43 @@
 package com.hcmus.wiberback.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 @Configuration
 public class WiberConfiguration {
+
+  private final RedisConnectionFactory redisConnectionFactory;
+
+  public WiberConfiguration(RedisConnectionFactory redisConnectionFactory) {
+    this.redisConnectionFactory = redisConnectionFactory;
+  }
 
   @Bean
   public BCryptPasswordEncoder bCryptPasswordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CacheManager cacheManager() {
+    RedisSerializationContext.SerializationPair<Object> jsonSerializer =
+        RedisSerializationContext.SerializationPair.fromSerializer(
+            new Jackson2JsonRedisSerializer<>(Object.class));
+
+    return RedisCacheManager.RedisCacheManagerBuilder
+        .fromConnectionFactory(redisConnectionFactory)
+        .cacheDefaults(
+            RedisCacheConfiguration
+                .defaultCacheConfig()
+                .serializeValuesWith(jsonSerializer))
+        .build();
   }
 }
