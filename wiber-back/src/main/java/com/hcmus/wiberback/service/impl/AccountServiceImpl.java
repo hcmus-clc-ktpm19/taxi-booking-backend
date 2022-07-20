6c0a8d61077738b34041a3eb49ca7803be130648
Nguyen Duc Nam <namworkmc@gmail.com>
2022-07-20 22:31:31 +0700

    Hot fix cannot get account from redis, add id to AccountDto

--- wiber-back/src/main/java/com/hcmus/wiberback/service/impl/AccountServiceImpl.java
+++ wiber-back/src/main/java/com/hcmus/wiberback/service/impl/AccountServiceImpl.java
@@ -9,7 +9,6 @@ import com.hcmus.wiberback.service.AccountService;
 import java.util.ArrayList;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -40,7 +39,7 @@ public class AccountServiceImpl implements AccountService, UserDetailsService {
   }
 
   @Override
-  @Cacheable(value = "account", key = "#phone", cacheManager = "cacheManager")
+//  @Cacheable(value = "accounts", key = "#phone") // TODO: Fix this later
   public Account findAccountByPhone(String phone) {
     return accountRepository.findAccountByPhone(phone)
         .orElseThrow(() -> new AccountNotFoundException("Account not found", phone));
