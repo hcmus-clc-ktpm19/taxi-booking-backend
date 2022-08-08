fcc5b1185e22384c98232a0ede5b0aac08d4d183
minhnhat02122001 <63940955+minhnhat02122001@users.noreply.github.com>
2022-08-08 22:39:23 +0700

    create queue to locate (#43)

--- wiber-back/src/main/java/com/hcmus/wiberback/repository/mongo/CarRequestRepository.java
+++ wiber-back/src/main/java/com/hcmus/wiberback/repository/mongo/CarRequestRepository.java
@@ -3,10 +3,12 @@ package com.hcmus.wiberback.repository.mongo;
 import com.hcmus.wiberback.model.entity.mongo.CarRequest;
 import com.hcmus.wiberback.model.entity.mongo.Customer;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRequestRepository extends MongoRepository<CarRequest, String> {
   List<CarRequest> findCarRequestsByCustomer(Customer customer);
+  List<CarRequest> findCarRequestByPickingAddress(String pickingAddress);
 }
