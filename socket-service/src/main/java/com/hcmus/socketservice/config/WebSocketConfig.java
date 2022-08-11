b2e3f816ae3e3e972449613e2df93e82cac80217
Hoàng Hữu Giáp <69495243+hhgiap241@users.noreply.github.com>
2022-08-11 21:47:39 +0700

    Refactor socket-service (#49)

--- socket-service/src/main/java/com/hcmus/socketservice/config/WebSocketConfig.java
+++ socket-service/src/main/java/com/hcmus/socketservice/config/WebSocketConfig.java
@@ -16,9 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // Configure global message broker, client will subscribe these message brokers to receive messages
-        // 统一配置消息代理，消息代理即订阅点，客户端通过订阅消息代理点接受消息
         registry.enableSimpleBroker("/b", "/g", "/user");
-
         registry.setUserDestinationPrefix("/user");
     }
 }
