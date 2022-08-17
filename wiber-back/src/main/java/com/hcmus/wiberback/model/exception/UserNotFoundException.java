70c4daf84c0c379885f6a47d5ab765aa97960905
Nguyen Duc Nam <66512409+namworkmc@users.noreply.github.com>
2022-08-17 21:17:45 +0700

    Migrate from get all requests by id to phone (#61)

--- wiber-back/src/main/java/com/hcmus/wiberback/model/exception/UserNotFoundException.java
+++ wiber-back/src/main/java/com/hcmus/wiberback/model/exception/UserNotFoundException.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 @Getter
 public class UserNotFoundException extends AbstractNotFoundException {
 
+  public static final String CUSTOMER_NOT_FOUND_MESSAGE = "Customer not found";
+
   public UserNotFoundException(String message, String id) {
     super(message, id);
   }
